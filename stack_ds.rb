49307ef0717e2aa319d6aa22a6be28eaa24694c6
yuriy.g <yuriygiy.g@gmail.com>
2021-10-30 10:22:00 +0300

    Updated API description

--- stack_ds.rb
+++ stack_ds.rb
@@ -1,41 +1,39 @@
 # linear data structure
 # Last In First Out (LIFO)
 
-# Push: Add an element to the top of a stack
-# Pop: Remove an element from the top of a stack
-# IsEmpty: Check if the stack is empty
-# IsFull: Check if the stack is full
-# Peek: Get the value of the top element without removing it
-
 class StackDs
-
 	attr_reader :stack, :size
 
 	def initialize(size = nil)
 		@stack = []
 		@size  = size
-	end 
+	end
 
+	# Push: Add an element to the top of a stack
 	def push(i)
 		is_full? ? stack : stack.push(i)
-	end 
+	end
 
+	# Pop: Remove an element from the top of a stack
 	def pop
-		stack.pop unless is_empty? 
-	end 
+		stack.pop unless is_empty?
+	end
 
+	# IsEmpty: Check if the stack is empty
 	def is_empty?
 		stack.empty?
-	end 
+	end
 
+	# IsFull: Check if the stack is full
 	def is_full?
-		stack.length == size if size   
-	end 
+		stack.length == size if size
+	end
 
+	# Peek: Get the value of the top element without removing it
 	def peek
-		stack.last unless is_empty? 
-	end 
-end 
+		stack.last unless is_empty?
+	end
+end
 
 # run 
 stack = StackDs.new(5)
@@ -51,4 +49,3 @@ p stack.is_full?
 
 p stack.peek
 p stack
-
